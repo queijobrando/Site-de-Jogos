a8b42f8b00b32af3ea0322cee22488f32e00cad6
tiago souza <tsouza@CASSIND.SE>
2025-02-27 13:34:55 -0300

    Adição formulário inserir jogos

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-class Game:
+class Game:#classe do jogo
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
+jogo1 = Game('Tetris', 'Puzzle', 'Atari')
+jogo2 = Game('God of War', 'Rack n Slash', 'PS2')
+
+lista = [jogo1, jogo2]  #lista do jogo
+
+#-----------------------------------------------------#
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    jogo1 = Game('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Game('God of War', 'Rack n Slash', 'PS2')
+def index():
+    return render_template('lista.html', titulo_site='Site de Jogos', titulo='Jogos', games = lista)
 
-    lista = [jogo1, jogo2]
-    return render_template('index.html', titulo_site='Jogos', games = lista)
+@app.route('/newgame') #pagina inserir novo jogo
+def newgame():
+    return render_template('newgame.html', titulo_site='Site de Jogos', titulo='Novo Jogo')
+
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form.get('nome')
+    category = request.form.get('categoria')
+    console = request.form.get('console')
+    jogo = Game(name, category, console) #cria um jogo novo
+    lista.append(jogo) #adiciona na lista de jogos
+    return render_template('lista.html', titulo_site='Site de Jogos', titulo='Jogos', games = lista)
     
 if __name__ == '__main__':
     app.run(debug=True)
